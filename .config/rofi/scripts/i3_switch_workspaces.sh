e0ccd21744b19372df6241fc61641dacaab2731e
Jesper Henriksen <jesper.henriksen@nxtech.no>
2024-03-13 16:44:13 +0100

    .

--- .config/rofi/scripts/i3_switch_workspaces.sh
+++ .config/rofi/scripts/i3_switch_workspaces.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ $# -eq 0 ]; then
-    i3-msg -t get_workspaces | tr ',' '\n' | grep "name" | sed 's/"name":"\(.*\)"/\1/g' | sort -n
+	i3-msg -t get_workspaces|jq -r '.[].name'|sort -n
 else
 	i3-msg workspace "$*" >/dev/null
 fi
